d41135a556f819f6421b90d7286634b6bf6682f2
Rambo <ramilsalihar@gmail.com>
2024-01-10 23:42:43 +0600

    application init

---
+++ lib/presentation/application/application.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HisMobile extends StatelessWidget {
+  const HisMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'His Mobile',
+    );
+  }
+}

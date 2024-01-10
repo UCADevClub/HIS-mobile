aad5742147742edc9415b7c11094690ad68ded0e
Rambo <ramilsalihar@gmail.com>
2024-01-11 00:04:26 +0600

    adding routes

--- lib/presentation/application/application.dart
+++ lib/presentation/application/application.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:his_mobile/presentation/core/theme/app_theme.dart';
+import 'package:his_mobile/presentation/routes/app_router.dart';
 
 class HisMobile extends StatelessWidget {
   const HisMobile({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final AppRouter appRouter = AppRouter();
     return MaterialApp.router(
       title: 'His Mobile',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+      routerConfig: appRouter.config(),
     );
   }
 }

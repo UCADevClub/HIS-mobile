c6c2c4ed751b401958c583c6ac1e3222f753ba93
Rambo <ramilsalihar@gmail.com>
2024-01-22 15:05:16 +0600

    updating authentication

--- lib/presentation/modules/auth_page/auth_page.dart
+++ lib/presentation/modules/auth_page/auth_page.dart
@@ -1,8 +1,10 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:his_mobile/presentation/core/validation/form_validation.dart';
 import 'package:his_mobile/presentation/core/widgets/app_button.dart';
 import 'package:his_mobile/presentation/core/widgets/forms/custom_text_form_field.dart';
+import 'package:his_mobile/presentation/modules/auth_page/bloc/auth_bloc.dart';
 
 @RoutePage()
 class AuthPage extends StatefulWidget {
@@ -31,30 +33,41 @@ class _AuthPageState extends State<AuthPage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
-          child: Form(
-            key: loginKey,
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  title: 'Login',
-                  controller: _loginController,
-                  validator: (_) => FormValidation.validateInn(
-                    _loginController.text,
+          child: BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              // TODO: implement listener
+            },
+            child: Form(
+              key: loginKey,
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    title: 'Login',
+                    controller: _loginController,
+                    validator: (_) =>
+                        FormValidation.validateInn(
+                          _loginController.text,
+                        ),
                   ),
-                ),
-                CustomTextFormField(
-                  title: 'Password',
-                  controller: _passwordController,
-                ),
-                AppButton(
-                  title: 'Login',
-                  onPressed: () {
-                    if (loginKey.currentState!.validate()) {
-                      print(_loginController.text);
-                    }
-                  },
-                ),
-              ],
+                  CustomTextFormField(
+                    title: 'Password',
+                    controller: _passwordController,
+                  ),
+                  AppButton(
+                    title: 'Login',
+                    onPressed: () {
+                      if (loginKey.currentState!.validate()) {
+                        BlocProvider.of<AuthBloc>(context).add(
+                          LoginRequested(
+                            // login: _loginController.text,
+                            // password: _passwordController.text,
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),

c40b97674d8187e9fe16d13a5b69fb5f52eaff5b
Rambo <ramilsalihar@gmail.com>
2024-01-23 14:24:47 +0600

    adding color

--- lib/presentation/core/theme/colors/dark_theme_colors.dart
+++ lib/presentation/core/theme/colors/dark_theme_colors.dart
@@ -6,15 +6,14 @@ class AppColorsDark extends AppColors {
   const AppColorsDark();
 
   @override
-  Color get black => throw UnimplementedError();
-
-  @override
   Color get primaryColor => throw UnimplementedError();
 
   @override
   Color get secondaryColor => throw UnimplementedError();
 
   @override
-  Color get white => throw UnimplementedError();
+  Color get white => const Color(0xffffffff);
 
+  @override
+  Color get black => const Color(0xff000000);
 }
--- lib/presentation/core/theme/colors/light_theme_colors.dart
+++ lib/presentation/core/theme/colors/light_theme_colors.dart
@@ -6,15 +6,15 @@ class AppColorsLight extends AppColors {
   const AppColorsLight();
 
   @override
-  Color get black => throw UnimplementedError();
-
-  @override
-  Color get primaryColor => throw UnimplementedError();
+  Color get primaryColor => const Color(0xff5ABDDA);
 
   @override
   Color get secondaryColor => throw UnimplementedError();
 
   @override
-  Color get white => throw UnimplementedError();
+  Color get white => const Color(0xffffffff);
+
+  @override
+  Color get black => const Color(0xff000000);
 
 }

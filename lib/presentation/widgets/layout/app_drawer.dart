1634bd8df74aaa7633899d3ac631e14a981256bb
Rambo <ramilsalihar@gmail.com>
2024-02-05 16:01:11 +0600

    adding switch button

--- lib/presentation/widgets/layout/app_drawer.dart
+++ lib/presentation/widgets/layout/app_drawer.dart
@@ -1,13 +1,20 @@
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:his_mobile/presentation/widgets/buttons/app_button.dart';
 
 @RoutePage()
-class AppDrawer extends StatelessWidget {
+class AppDrawer extends StatefulWidget {
   const AppDrawer({super.key});
 
   @override
+  State<AppDrawer> createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  @override
   Widget build(BuildContext context) {
+    var current = 0;
     return Drawer(
       child: Column(
         children: [
@@ -43,9 +50,44 @@ class AppDrawer extends StatelessWidget {
                       fontSize: 20,
                     ),
                   ),
+                  const SizedBox(height: 10),
                 ],
               ),
             ),
+          ),
+          AnimatedToggleSwitch<int>.size(
+            textDirection: TextDirection.rtl,
+            current: current,
+            values: const [0, 1],
+            iconOpacity: 0.2,
+            indicatorSize: const Size.fromWidth(100),
+            iconBuilder: (index) => index == 0
+                ? const Icon(
+                    Icons.home,
+                    size: 30,
+                    color: Colors.white,
+                  )
+                : const Icon(
+                    Icons.settings,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+            borderWidth: 4.0,
+            iconAnimationType: AnimationType.onHover,
+            style: ToggleStyle(
+              borderColor: Colors.transparent,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: Offset(0, 1.5),
+                ),
+              ],
+            ),
+            onChanged: (int i) => setState(() => current = i),
+            onTap: (i) => print('Tapped on $i'),
           ),
           TextButton(
             onPressed: () {
